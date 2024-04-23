d0d43f35107c35f44e236ecf7f00413954d1f0ef
HPBhikadiya <hirenpbhikadiya@gmail.com>
2024-04-23 11:30:03 +0530

    email lower case added

--- src/modules/user/getUserByEmail.ts
+++ src/modules/user/getUserByEmail.ts
@@ -7,6 +7,8 @@ import { UserModel } from "./schema";
  * @returns null or User class
  */
 export const getUserByEmail = async (email: string) => {
-  const user = await UserModel.findOne({ email });
+  const user = await UserModel.findOne({
+    email: { $regex: new RegExp(`^${email}$`), $options: "i" },
+  });
   return user ? new User(user) : null;
 };
