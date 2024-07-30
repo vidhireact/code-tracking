669880f53ed9d61ca0a3f15d0b381bcbd120fcd5
Krunal <>
2024-07-30 17:17:29 +0530

    Create API Wait-While user

--- src/controller/user/controller.ts
+++ src/controller/user/controller.ts
@@ -1,9 +1,9 @@
 import { Response } from "express";
 import { Request } from "./../../request";
 import Joi from "joi";
-import { get as _get } from "lodash";
-import { AES, enc } from "crypto-js";
-
+import { get as _get, uniqBy as _uniqBy } from "lodash";
+import { AES } from "crypto-js";
+import axios from "axios";
 import {
   getPopulatedUser,
   getUserById,
@@ -13,7 +13,17 @@ import {
   User,
 } from "../../modules/user";
 
-import { getWaitWhileUserByBusinessId } from "../../modules/waitWhileUser";
+import {
+  getWaitWhileUserByBusinessId,
+  getWaitWhileUserByEmail,
+  saveWaitWhileUser,
+  WaitWhileUser,
+} from "../../modules/waitWhileUser";
+import {
+  Business,
+  getBusinessById,
+  updateBusiness,
+} from "../../modules/business";
 
 export default class Controller {
   protected readonly updateUserSchema = Joi.object({
@@ -65,6 +75,113 @@ export default class Controller {
         return AES.encrypt(v, process.env.JWT_PASSWORD_SECRET).toString();
       }),
   });
+
+  protected readonly createUserSchema = Joi.object({
+    businessId: Joi.string().required(),
+    name: Joi.string().required(),
+    email: Joi.string()
+      .email()
+      .required()
+      .external(async (v: string) => {
+        if (!v) return v;
+        const checkEmail = await getWaitWhileUserByEmail(v);
+        if (checkEmail) {
+          throw new Error("Please provide valid waitWhile User Email");
+        }
+        return v;
+      }),
+    password: Joi.string()
+      .required()
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/),
+    roles: Joi.array()
+      .items(
+        Joi.string().valid("admin", "editor", "reader", "owner").lowercase()
+      )
+      .required()
+      .max(4),
+  });
+
+  protected readonly create = async (req: Request, res: Response) => {
+    try {
+      const payload = req.body;
+
+      const payloadValue = await this.createUserSchema
+        .validateAsync(payload)
+        .then((value) => {
+          return value;
+        })
+        .catch((e) => {
+          console.log(e);
+          res.status(422).json({ message: e.message });
+          return;
+        });
+
+      if (!payloadValue) {
+        return;
+      }
+
+      const getBusiness = await getBusinessById(payloadValue.businessId);
+
+      const waitWhileApiKey = process.env.WAIT_WHILE_KEY;
+
+      const option = {
+        url: `${process.env.WAITWHILE_BASE_URL}/users`,
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "content-type": "application/json",
+          apikey: `${waitWhileApiKey}`,
+        },
+        data: JSON.stringify({
+          name: payloadValue.name,
+          email: payloadValue.email,
+          password: payloadValue.password,
+          locationIds: [getBusiness.waitWhileLocationId],
+          roles: payloadValue.roles,
+        }),
+      };
+
+      await axios(option);
+
+      const password = AES.encrypt(
+        payloadValue.password,
+        process.env.AES_KEY
+      ).toString();
+
+      await saveWaitWhileUser(
+        new WaitWhileUser({
+          ...payloadValue,
+          password: password,
+        })
+      ).catch((e) => {
+        console.log(e);
+        res.status(422).json({ message: e.message });
+        return;
+      });
+
+      const getWaitWhileUser = await getWaitWhileUserByEmail(
+        payloadValue.email
+      );
+
+      await updateBusiness(
+        new Business({
+          ...getBusiness,
+          waitWhileUserId: _uniqBy(
+            [...getBusiness.waitWhileUserId, getWaitWhileUser._id],
+            (_id) => _id.toString()
+          ),
+        })
+      );
+
+      res.status(200).json(getWaitWhileUser);
+    } catch (error) {
+      console.log("error", "error in create user", error);
+      res.status(500).json({
+        message: "Hmm... Something went wrong. Please try again later.",
+        error: _get(error, "message"),
+      });
+    }
+  };
 
   protected readonly updateUser = async (req: Request, res: Response) => {
     try {
@@ -141,17 +258,19 @@ export default class Controller {
     }
   };
 
-  protected readonly getUserByBusinessId = async (req: Request, res: Response) => {
+  protected readonly getUserByBusinessId = async (
+    req: Request,
+    res: Response
+  ) => {
     try {
       const { businessId } = req.params;
-      if(!businessId){
+      if (!businessId) {
         res.status(422).json({ message: "Invalid Business ID." });
         return;
       }
 
       const user = await getWaitWhileUserByBusinessId(businessId);
       res.status(200).json(user);
-
     } catch (error) {
       console.log("error", "error in get getUserByBusinessId", error);
       res.status(500).json({
